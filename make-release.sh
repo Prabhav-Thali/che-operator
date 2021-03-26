d8481188feb387d2329e08ffd18ed18037263a97
Prabhav Thali <Prabhav.Thali1@ibm.com>
2021-03-26 15:40:41 +0530

    Add personal repository

--- make-release.sh
+++ make-release.sh
@@ -16,7 +16,7 @@ init() {
   RELEASE="$1"
   BRANCH=$(echo $RELEASE | sed 's/.$/x/')
   RELEASE_BRANCH="${RELEASE}-release"
-  GIT_REMOTE_UPSTREAM="https://github.com/eclipse-che/che-operator.git"
+  GIT_REMOTE_UPSTREAM="https://github.com/Prabhav-Thali/che-operator.git"
   RUN_RELEASE=false
   PUSH_OLM_BUNDLES=false
   PUSH_GIT_CHANGES=false
