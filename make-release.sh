db97168ea3ae91ac816accf30668c4ae75f1b90e
Prabhav Thali <Prabhav.Thali1@ibm.com>
2021-03-26 14:15:35 +0530

    implemented crazy-max buildx

--- make-release.sh
+++ make-release.sh
@@ -26,7 +26,7 @@ init() {
   PREPARE_COMMUNITY_OPERATORS_UPDATE=false
   RELEASE_DIR=$(cd "$(dirname "$0")"; pwd)
   FORCE_UPDATE=""
-  BUILDX_PLATFORMS="linux/amd64,linux/ppc64le"
+  BUILDX_PLATFORMS="linux/amd64,linux/ppc64le,linux/s390x"
   DEV_WORKSPACE_CONTROLLER_VERSION="master"
   DEV_WORKSPACE_CHE_OPERATOR_VERSION="main"
 
@@ -163,7 +163,7 @@ releaseOperatorCode() {
   docker login quay.io -u "${QUAY_ECLIPSE_CHE_USERNAME}" -p "${QUAY_ECLIPSE_CHE_PASSWORD}"
 
   echo "[INFO] releaseOperatorCode :: Build operator image in platforms: $BUILDX_PLATFORMS"
-  docker buildx build --build-arg DEV_WORKSPACE_CONTROLLER_VERSION=${DEV_WORKSPACE_CONTROLLER_VERSION} --build-arg DEV_WORKSPACE_CHE_OPERATOR_VERSION=${DEV_WORKSPACE_CHE_OPERATOR_VERSION} --platform "$BUILDX_PLATFORMS" --push -t "quay.io/eclipse/che-operator:${RELEASE}" .
+  docker buildx build --build-arg DEV_WORKSPACE_CONTROLLER_VERSION=${DEV_WORKSPACE_CONTROLLER_VERSION} --build-arg DEV_WORKSPACE_CHE_OPERATOR_VERSION=${DEV_WORKSPACE_CHE_OPERATOR_VERSION} --platform "$BUILDX_PLATFORMS" --push -t "quay.io/prabhav/che-operator:${RELEASE}" .
 }
 
 updateNightlyOlmFiles() {
